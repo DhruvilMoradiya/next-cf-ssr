2793f975e06eabbc034571bcefb94a582610cc10
DhruvilMoradiya <dhruvil1808@gmail.com>
2023-04-28 13:55:45 +0530

    JAI SHREE RAM

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,12 +1,15 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 
-export async function getServerSideProps() {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  // add Cache-Control HTTP Header to response
+  ctx.res.setHeader("age", 1000);
   return {
     props: {
-      time: new Date().toString(),
-    }, // will be passed to the page component as props
+      time: new Date().toISOString(),
+    },
   };
-}
+};
 export default function Home({ time }) {
   return (
     <>
@@ -16,7 +19,7 @@ export default function Home({ time }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Only SSR</h1>
+      <h1>SSR with Caching</h1>
       <h3>{time}</h3>
     </>
   );
